de64832b748f75866a7d97bf52fab0703a119972
KeerthiPasala <kpasa3@unh.newhaven.edu>
2021-12-08 00:35:05 -0500

    local changes

---
+++ src/components/Home.vue
@@ -0,0 +1,134 @@
+<template>
+  <body>
+    <nav class="navbar" id="myNavbar">
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">Aboutme</a></li>
+        <li><a href="#papers">Papers</a></li>
+        <li><a href="#experiences">Experiences</a></li>
+        <li><a href="#hobbies">Hobbies</a></li>
+        <li><a href="#blog">Blog</a></li>
+    </ul>
+    </nav>
+    <section id="home">
+        <div class="main">
+            <h1 class="headings">I AM <br> ALBERT EINSTEIN</h1>
+        </div>
+    </section>
+    <section id="about">
+        <h1 class="headings">ABOUT ME</h1>
+        <div id="pic">
+            <img src="../assets/logo3.jpg" alt="">
+            <div id="introduction">
+                <h2>ALBERT EINSTEIN</h2>
+                <p>Albert Einstein was born at Ulm, in Württemberg, Germany, on March 14, 1879. Six weeks later the family moved to Munich, where he later on began his schooling at the Luitpold Gymnasium. 
+                    Later, they moved to Italy and Albert continued his education at Aarau, Switzerland and in 1896 he entered the Swiss Federal Polytechnic School in Zurich to be trained as a teacher in physics and mathematics.
+                    In 1901, the year he gained his diploma, he acquired Swiss citizenship and, as he was unable to find a teaching post, he accepted a position as technical assistant in the Swiss Patent Office. 
+                    In 1905 he obtained his doctor’s degree.
+                    During his stay at the Patent Office, and in his spare time, he produced much of his remarkable work and in 1908 he was appointed Privatdozent in Berne. 
+                    In 1909 he became Professor Extraordinary at Zurich, in 1911 Professor of Theoretical Physics at Prague, returning to Zurich in the following year to fill a similar post.
+                     In 1914 he was appointed Director of the Kaiser Wilhelm Physical Institute and Professor in the University of Berlin. 
+                    He became a German citizen in 1914 and remained in Berlin until 1933 when he renounced his citizenship for political reasons and emigrated to America to take the position of Professor of Theoretical Physics at Princeton.
+                    He became a United States citizen in 1940 and retired from his post in 1945.
+                    After World War II, Einstein was a leading figure in the World Government Movement, he was offered the Presidency of the State of Israel, which he declined, and he collaborated with Dr. Chaim Weizmann in establishing the Hebrew University of Jerusalem.
+                    Albert Einstein was a German-born theoretical physicist, widely acknowledged to be one of the greatest physicists of all time. 
+                    Einstein is best known for developing the theory of relativity, but he also made important contributions to the development of the theory of quantum mechanics.
+                </p>                             
+            </div>
+        </div>
+    </section>
+    <section id="papers">
+        <h1 class="headings">Papers</h1>
+        <div class="pr1">
+            <ol>
+            <li>Volume 1: The Early Years, 1879-1902 : <a href="https://einsteinpapers.press.princeton.edu/vol1-doc/">Click here</a></li>
+            <li>Volume 2: The Swiss Years: Writings, 1900-1909<a href="https://einsteinpapers.press.princeton.edu/vol2-doc/">Click here</a></li>
+            <li>Volume 3: The Swiss Years: Writings 1909-1911<a href="https://einsteinpapers.press.princeton.edu/vol3-doc/">Click here</a></li>
+            <li>Volume 4: The Swiss Years: Writings 1912-1914<a href="https://einsteinpapers.press.princeton.edu/vol4-doc/">Click here</a></li>
+            <li>Volume 5: The Swiss Years: Correspondence, 1902-1914<a href="https://einsteinpapers.press.princeton.edu/vol5-doc/">Click here</a></li>
+            <li>Volume 6: The Berlin Years: Writings, 1914-1917<a href="https://einsteinpapers.press.princeton.edu/vol6-doc/">Click here</a></li>
+            <li>Volume 7: The Berlin Years: Writings, 1918-1921<a href="https://einsteinpapers.press.princeton.edu/vol7-doc/">Click here</a></li>
+            <li>Volume 8, Part A: The Berlin Years: Correspondence 1914-1917<a href="https://einsteinpapers.press.princeton.edu/vol8a-doc/">Click here</a></li>
+            <li>Volume 8, Part B: The Berlin Years: Correspondence 1918<a href="https://einsteinpapers.press.princeton.edu/vol8b-doc/">Click here</a></li>
+            <li>Volume 9: The Berlin Years: Correspondence January 1919-April 1920<a href="https://einsteinpapers.press.princeton.edu/vol9-doc/">Click here</a></li>
+            <li>Volume 10: The Berlin Years: Correspondence May-December 1920 / Supplementary Correspondence 1909-1920<a href="https://einsteinpapers.press.princeton.edu/vol10-doc/">Click here</a></li>
+            <li>Volume 11: Cumulative Index, Bibliography, List of Correspondence, Chronology, and Errata to Volumes 1-10<a href="https://einsteinpapers.press.princeton.edu/vol11-doc/">Click here</a></li>
+            <li>Volume 12: The Berlin Years: Correspondence January-December 1921<a href="https://einsteinpapers.press.princeton.edu/vol12-doc/">Click here</a></li>
+            <li>Volume 13: The Berlin Years: Writings & Correspondence January 1922-March 1923<a href="https://einsteinpapers.press.princeton.edu/vol13-doc/">Click here</a></li>
+            <li>Volume 14: The Berlin Years: Writings & Correspondence, April 1923-May 1925<a href="https://einsteinpapers.press.princeton.edu/vol14-doc/">Click here</a></li>
+            <li>Volume 15: The Berlin Years: Writings & Correspondence, June 1925-May 1927<a href="https://einsteinpapers.press.princeton.edu/vol15-doc/">Click here</a></li>
+        </ol>
+        </div> 
+    </section>
+<section id="experiences">
+     <h1 class="headings">Experiences</h1>
+     <div id="pic1">
+        <img src="../assets/logo4.jpg" alt="">
+         <div id="introduction">
+             <h2>Einstein's Experience</h2>
+            <ul>
+                <li>Clerk, Swiss Patent Office, Bern, 1902-1909</li>
+                <li>Lecturer, University of Bern, 1908. </li>
+                <li>Associate professor of physics, University of Zurich, 1909-1910.</li>
+                <li>Professor of physics, Unversity of Prague, 1911.</li>
+                <li>Professor of physics, Swiss Federal Polytechnic Institute, 1912-1913.</li>
+                <li>Director, Kaiser Wilhelm Physical Institution, Berlin, 1914-1933.</li>
+                <li>Professor of Theoretical Physics, Institute for Advanced Study, Princeton, 1933-1955.</li>
+            </ul>
+        </div>
+    </div>
+  </section>
+<section id="hobbies">
+    <h1 class="headings">Hobbies</h1>
+    <div class="hob">
+      <div class="hob1">
+       <img src="../assets/hobby2.jpg" alt="">
+        <h2>He loved to sail</h2>
+            <p>
+                When Einstein attended college at the Polytechnic Institute in Zurich, Switzerland, he fell in love with sailing. He would often take a boat out onto a lake, pull out a notebook, relax, and think. 
+                Even though Einstein never learned to swim, he kept sailing as a hobby throughout his life.
+            </p>
+       </div> 
+       <div class="hob1">
+          <img src="../assets/hobby1.png" alt="">
+            <h2>Einstein and the Violin</h2>
+            <p>When Einstein was 13-years old, he suddenly changed his mind about the violin when he heard the music of Mozart. With a new passion for playing, Einstein continued to play the violin until the last few years of his life.</p>
+        </div>
+         <div class="hob1">
+            <img src="../assets/hobby3.jpg" alt="">
+                <h2>He loved reading books</h2>
+                <p>Einstein enjoyed reading and is quoted as saying that "Traktat" by David Humes had a big influence on him. He enjoyed classics, such as Don Quijote by Cervantes Saavedra and books with a scientific bent, including "Energy and Matter" by Ludwig Büchners and Aaron Bernstein's "Natural-Scientific Popular Books." He also read books by the philosophers Spinoza and Schopenhauer.
+                 </p>
+        </div>
+    </div> 
+</section> 
+   <section id="blog">
+    <h1 class="headings">Blog</h1>
+    <div class="hob">
+        <div class="blog1">
+            <img src="../assets/stamps.jpg" alt="">
+        </div> 
+        <div class="blog1">
+            <img src="../assets/img1.jpg" alt="">
+        </div>
+        <div class="blog1">
+            <img src="../assets/img2.jpg" alt="">
+        </div>
+    </div>
+    <div class="hob">
+        <div class="blog1">
+            <img src="../assets/img3.jpg" alt="">
+        </div>
+        <div class="blog1">
+            <img src="../assets/img5.png" alt="">
+        </div>
+        <div class="blog1">
+            <img src="../assets/img6.png" alt="">
+        </div>
+    </div>
+   </section> 
+    <footer>
+       <div>@copyright; 2021</div>
+   </footer>   
+</body>
+</template> 
